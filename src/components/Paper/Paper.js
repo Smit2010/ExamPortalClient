ed497c55e99c3366531589b4a212b84b8a0bb395
harshil1111999 <38987193+harshil1111999@users.noreply.github.com>
2020-08-16 18:23:58 +0530

    Update Paper.js

--- src/components/Paper/Paper.js
+++ src/components/Paper/Paper.js
@@ -27,25 +27,64 @@ class Paper extends Component {
         let diagram = []
         Array.from(this.props.question_set.values()).map(element => {
             if(element.type === "SINGLE") {
-                single.push(element)
+                single.push(element.id)
             } else if(element.type === "MULTIPLE") {
-                multiple.push(element)
+                multiple.push(element.id)
             } else if(element.type === "SUBJECTIVE") {
-                subjective.push(element)
+                subjective.push(element.id)
             } else if(element.type === "DIAGRAM") {
-                diagram.push(element)
+                diagram.push(element.id)
             }
         })
 
-        let count = 0, total = 1
-        function incr() {total = total + multiple.length}
-        function calcId(type, num1,num2) {
-            let ans1, ans2
-            if(type === "multiple") {
-                ans1 = multiple[num1-1].id
-                ans2 = multiple[num2-1].id
-            } else {
-
+        let count = 0
+        // function incr(val) {total = total + val}
+        function calcId(type, id, flag) {
+            let ans1 = id, ans2 = id
+            if(type === "MULTIPLE") {
+                let index = multiple.indexOf(id)
+                if(flag) {
+                    if(index > 0) {
+                        ans2 = multiple[index - 1]
+                    }
+                } else {
+                    if(index < multiple.length - 1) {
+                        ans2 = multiple[index + 1]
+                    }
+                }      
+            } else if(type === "SINGLE"){
+                let index = single.indexOf(id)
+                if(flag) {
+                    if(index > 0) {
+                        ans2 = single[index - 1]
+                    }
+                } else {
+                    if(index < single.length - 1) {
+                        ans2 = single[index + 1]
+                    }
+                } 
+            } else if(type === "SUBJECTIVE"){
+                let index = subjective.indexOf(id)
+                if(flag) {
+                    if(index > 0) {
+                        ans2 = subjective[index - 1]
+                    }
+                } else {
+                    if(index < subjective.length - 1) {
+                        ans2 = subjective[index + 1]
+                    }
+                } 
+            } else if(type === "DIAGRAM"){
+                let index = diagram.indexOf(id)
+                if(flag) {
+                    if(index > 0) {
+                        ans2 = diagram[index - 1]
+                    }
+                } else {
+                    if(index < diagram.length - 1) {
+                        ans2 = diagram[index + 1]
+                    }
+                } 
             }
             return [ans1, ans2]
         }
@@ -63,12 +102,10 @@ class Paper extends Component {
                         <Divider />
                             {multiple.map(elem => {
                                 count = count + 1
-                                return <QuestionCardMultiple calcId={calcId} show={true} id={elem.id} len={multiple.length} num={count} total={total} output={elem.output} optionList={elem.optionList}/>
+                                return <QuestionCardMultiple calcId={calcId} show={true} id={elem} num={count} output={this.props.question_set.get(elem.toString()).output} optionList={this.props.question_set.get(elem.toString()).optionList}/>
                             })
                         }
                     </div>}
-
-                    {incr()}
 
                     {/* Show single choice questions */}
                     {single.length > 0 && <div className="box">
@@ -76,12 +113,10 @@ class Paper extends Component {
                         <Divider />
                             {single.map(elem => {
                                 count = count + 1
-                                return <QuestionCardSingle calcId={calcId} show={true} id={elem.id} len={single.length} num={count} total={total} output={elem.output} optionList={elem.optionList}/>
+                                return <QuestionCardSingle calcId={calcId} show={true} id={elem} num={count} output={this.props.question_set.get(elem.toString()).output} optionList={this.props.question_set.get(elem.toString()).optionList}/>
                             })
                         }
                     </div>}
-
-                    {incr()}
 
                     {/* Show subjective questions */}
 
@@ -90,12 +125,10 @@ class Paper extends Component {
                         <Divider />
                             {subjective.map(elem => {
                                 count = count + 1
-                                return <QuestionCardSubjective calcId={calcId} show={true} id={elem.id} len={subjective.length} num={count} total={total} output={elem.output} optionList={elem.optionList}/>
+                                return <QuestionCardSubjective calcId={calcId} show={true} id={elem} num={count} output={this.props.question_set.get(elem.toString()).output} optionList={this.props.question_set.get(elem.toString()).optionList}/>
                             })
                         }
                     </div>}
-
-                    {incr()}
 
                     {/* Show diagram based questions */}
 
@@ -104,7 +137,7 @@ class Paper extends Component {
                         <Divider />
                             {diagram.map(elem => {
                                 count = count + 1
-                                return <QuestionCardDiagram calcId={calcId} show={true} id={elem.id} len={diagram.length} num={count} total={total} output={elem.output} optionList={elem.optionList}/>
+                                return <QuestionCardDiagram calcId={calcId} show={true} id={elem} num={count} output={this.props.question_set.get(elem.toString()).output} optionList={this.props.question_set.get(elem.toString()).optionList}/>
                             })
                         }
                     </div>}
@@ -123,7 +156,8 @@ class Paper extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        question_set: state.question.question_set
+        question_set: state.question.question_set,
+        currQuestionId: state.question.currQuestionId
     }
 }
 
