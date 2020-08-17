8de73bed328a8e6a06b28283f7b98d55de72632c
harshil1111999 <38987193+harshil1111999@users.noreply.github.com>
2020-08-18 02:03:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -28,9 +28,6 @@ class App extends React.Component{
                             <Route exact path="/">
                                 <Redirect to="/home"/>
                             </Route>
-                            <Route path="/edit-question">
-                                <EditQuestion />
-                            </Route>
                             <Route path="/question-paper">
                                 <Paper />
                             </Route>
