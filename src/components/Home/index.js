d096946fb741bd8200bec03ebc7af007b8f4c205
harshil1111999 <38987193+harshil1111999@users.noreply.github.com>
2020-08-22 22:57:48 +0530

    Add files via upload

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-import { withRouter, Link } from "react-router-dom";
+import { withRouter, Link, Redirect } from "react-router-dom";
 import Welcome from '../Welcome/Welcome.js';
 
 class LandingPage extends React.Component{
-
-    AuthenticatedUser = () => {
-        return [
-            <div className="column is-offset-5 is-centered is-2">
-                <div class="control is-centered">
-                    <button class="button is-rounded is-centered is-fullwidth" type="submit" style={{backgroundColor: "DarkSlateBlue"}}>
-                    <p style={{color: "white"}}>Go to Chats</p>
-                    </button>
-                </div>
-            </div>,
-        ];
-    }
     
     NonAuthenticatedUser = () => {
         return [
@@ -32,12 +20,12 @@ class LandingPage extends React.Component{
     render(){
         const isAuthenticated = this.props.isAuthenticated;
         return(
-            <div style={{backgroundColor: "#d6dbd7", height:"100vh"}}>
+            <div style={{backgroundColor: "#fff", height:"100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
                 <div className="container" style={{paddingTop: 20}}>
                     <div className="column is-centered">
                         <Welcome/>
                     </div>
-                    {isAuthenticated ? this.AuthenticatedUser() : this.NonAuthenticatedUser()}
+                    {isAuthenticated ? <Redirect to="/dashboard"/> : this.NonAuthenticatedUser()}
                 </div>
             </div>
         );
